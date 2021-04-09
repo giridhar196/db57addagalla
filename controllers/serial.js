3952426485c8c3920fb0b9ff4138606553d445ea
Giridhar Addagalla <giridhar196@gmail.com>
2021-04-09 13:49:07 -0500

    assignment 12 start

--- controllers/serial.js
+++ controllers/serial.js
@@ -10,10 +10,7 @@ exports.serial_list = async function (req, res) {
     }
 };
 
-// for a specific serial.
-exports.serial_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: serial detail: ' + req.params.id);
-};
+
 // Handle serial create on POST.
 exports.serial_create_post = async function (req, res) {
     console.log(req.body)
@@ -21,7 +18,7 @@ exports.serial_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costumetype":"goat", "cost":12, "size":"large"}
+    // {"serialtype":"goat", "cost":12, "size":"large"}
     document.name = req.body.name;
     document.author = req.body.author;
     document.season = req.body.season;
@@ -32,13 +29,51 @@ exports.serial_create_post = async function (req, res) {
         res.error(500, `{"error": ${err}}`);
     }
 };
+// for a specific serial.
+exports.serial_detail = async function (req, res) {
+    console.log("I am here");
+    console.log("detail" + req.params.id)
+    try {
+        result = await serial.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
 // Handle serial delete form on DELETE.
 exports.serial_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: serial delete DELETE ' + req.params.id);
 };
 // Handle serial update form on PUT.
-exports.serial_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: serial update PUT' + req.params.id);
+exports.serial_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await serial.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.name){
+            toUpdate.name = req.body.name;
+        } else{
+
+        }
+        if (req.body.author){
+            toUpdate.author = req.body.author;
+        }else{
+
+        }
+        if (req.body.season){
+            toUpdate.season = req.body.season;
+        } else{
+
+        }
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
+
 };
 // VIEWS
 // Handle a show all view
