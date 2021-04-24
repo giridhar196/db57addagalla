5ed6e27b5e1d80191201ec0368222e31ab0da97c
Giridhar Addagalla <giridhar196@gmail.com>
2021-04-23 23:33:49 -0500

    bonus screen shots

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ async function recreateDB() {
 }
 let reseed = true;
 if (reseed) {
-  recreateDB();
+ recreateDB();
 }
 var app = express();
 
--- models/serial.js
+++ models/serial.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const serialSchema = mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        require: [true, "Name is required"]
+    },
     author: String,
-    season: Number
+    season: {
+        type: Number,
+        min: [2, "Seson should be min 2"],
+        max: [10, "Season should be max of 10"]
+    }
 })
 module.exports = mongoose.model("Serial", serialSchema)
